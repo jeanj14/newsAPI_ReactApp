f10d294edbd521de1a66da11f851cfd4628bae34
Jennifer <bobj14022001@gmail.com>
2025-04-29 11:31:12 +0200

    add tanstack query and axios

--- src/components/Articles/Article.jsx
+++ src/components/Articles/Article.jsx
@@ -3,11 +3,11 @@ import ArticleContent from "./ArticleContent";
 function Article(props){
     // const source = props.source;
     // console.log(source)
-    // const category = props.category;
+    const category = props.category;
     // const sourceArticle = source[0];
     const type = props.type;
-    return <div className={`article-${type} flex flex-col justify-end`}>
-        <ArticleContent type={type} />
+    return <div className={`article-${type} flex flex-col justify-center`}>
+        <ArticleContent type={type} category={category} />
     </div>
 }
 
--- src/components/Articles/ArticleContent.jsx
+++ src/components/Articles/ArticleContent.jsx
@@ -1,10 +1,41 @@
+import { getNews } from "@api/index"
+import { useQuery } from "@tanstack/react-query";
+import placeholder from '@assets/placeholder.jpg'
+
 function ArticleContent(props){
-    // const article = props.article;
-    return <div className="px-5 pb-2.5 flex flex-col">
-        <p className="text-sm">Article Genre</p>
-            <h1 className={`title-${props.type} max-w-full truncate`}>Title</h1>
-        <p>Author - Published At</p>
+    const category = props.category;
+    const { data, isLoading } = useQuery({
+        queryKey: ["articles", category],
+        queryFn: async () => {
+            return getNews(category)
+        }
+    })
+    if (isLoading){
+        return <h1>LOADING....</h1>
+    } 
+    else {
+        const randomIndex = Math.floor(Math.random() * data.length);
+        const article = data[randomIndex];
+        const author = article.author ?? article.source.name;
+        const articleBg = article.urlToImage ?? placeholder;
+        const publishedAt = article.publishedAt.slice(0, 10);
+        const bgStyle = {
+            backgroundImage: `url(${articleBg})`,
+            backgroundRepeat: 'no-repeat',
+            backgroundSize: 'cover',
+            height: `100%`
+
+        };
+        console.log(article);
+        console.log(article.urlToImage);
+        return <div className="flex flex-col justify-end" style={bgStyle} >
+            <div className="bg-stone-950/50 mask-t-from-85% w-full pt-5 px-2.5">
+            <p className="text-sm text-white z-0">{article.category}</p>
+            <h1 className={`text-white title-${props.type} max-w-full truncate`}>{article.title}</h1>
+        <p className="text-white">{author} - {publishedAt}</p>
+            </div>
     </div>
+    }
 }
 
 export default ArticleContent;
