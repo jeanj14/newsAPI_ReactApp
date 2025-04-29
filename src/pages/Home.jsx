dbd2b8a39201df173a23b6d00b101e4bfe2e11cd
Jennifer <bobj14022001@gmail.com>
2025-04-29 11:33:57 +0200

    add styling for articles

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,21 +1,17 @@
 import Article from "@articles/Article"
 import { useEffect, useState } from "react"
-// import { topTech, topEntertainment, topSports } from "@api/index"
+
 
 /* useState to store site values from API rendered on the websites */
 function Home(){
-
-    useEffect(() => {
-        console.log("Page loading")
-    }, [])
     // console.log(topTech);
     return <div className="h-screen bg-green-500">
         <div className="w-full h-1/2 p-2.5">
-        <Article type="main" />
+        <Article type="main" category="business"/>
         </div>        
         <div className="grid grid-cols-1 sm:grid-cols-2 h-1/2 p-2.5">
-        <Article type="side"  />
-        <Article type="side" />
+        <Article type="side" category="technology" />
+        <Article type="side" category="sports" />
         </div>
     </div>
 }
--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,38 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import { resolve } from "path";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(),
-    tailwindcss(),
-  ],
-  resolve: {
-    alias: {
-      "@app": resolve("src"),
-      "@assets": resolve("src/assets"),
-      "@components": resolve("src/components"),
-      "@header": resolve("src/components/Header"),
-      "@footer": resolve("src/components/Footer"),
-      "@articles": resolve("src/components/Articles"),
-      "@pages": resolve("src/pages"),
-      "@api": resolve("src/api"),
-      // "@store": resolve("src/store"),
-      // "@ui": resolve("src/ui"),
-      // "@utils": resolve("src/utils"),
+
+
+export default defineConfig(({ mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  // Set the third parameter to '' to load all env regardless of the
+  // `VITE_` prefix.
+
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    // vite config
+    plugins: [react(), tailwindcss()],
+    resolve: {
+      alias: {
+        "@app": resolve("src"),
+        "@assets": resolve("src/assets"),
+        "@components": resolve("src/components"),
+        "@header": resolve("src/components/Header"),
+        "@footer": resolve("src/components/Footer"),
+        "@articles": resolve("src/components/Articles"),
+        "@pages": resolve("src/pages"),
+        "@api": resolve("src/api"),
+        // "@store": resolve("src/store"),
+        // "@ui": resolve("src/ui"),
+        // "@utils": resolve("src/utils"),
+      },
+    
     },
-  
-  },
+    define: {
+      __VITE_API_KEY__: JSON.stringify(env.VITE_API_KEY),
+    },
+  }
 })
