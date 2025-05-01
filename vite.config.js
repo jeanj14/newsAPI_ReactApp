22302c1a718168a01353afdde72f10a9bea1f12a
Jennifer <bobj14022001@gmail.com>
2025-05-01 18:50:49 +0200

    add vercel plugin

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import vercel from 'vite-plugin-vercel';
 import { resolve } from "path";
 
 // https://vite.dev/config/
@@ -14,7 +15,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
   return {
     // vite config
-    plugins: [react(), tailwindcss()],
+    plugins: [react(), tailwindcss(), vercel()],
     build:{
       target: "esnext", // or "es2019",
     },
