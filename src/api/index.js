6903f829fbf59fc42f07fb1f0d3b7e1a7720c03f
Jennifer <bobj14022001@gmail.com>
2025-05-01 16:56:23 +0200

    switch from axios to fetch

--- src/api/index.js
+++ src/api/index.js
@@ -6,13 +6,16 @@ const NEWS_API_URL = `https://newsapi.org/v2`
 //function to fetch data from NewsAPI using axios
 const getNews = async (category) => {
   try {
-    const response = await axios.get(`${NEWS_API_URL}/top-headlines?category=${category}&apiKey=${API_KEY}`);
-    const data = response.data;
+    // const response = await axios.get(`${NEWS_API_URL}/top-headlines?category=${category}&apiKey=${API_KEY}`);
+    const response = await fetch(`${NEWS_API_URL}/top-headlines?category=${category}&apiKey=${API_KEY}`)
+    const data = await response.json() 
     const articles = data.articles;
     return articles.map( (article) => ({...article, category}) ); //category is attached with map() to each news article to help
   } catch (error) {
     return error
   }
 }
+
+console.log(await getNews("business"))
 
 export default getNews  ;
